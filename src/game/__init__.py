c05fd18c6f9d999114385a69729407ed4980fe5c
J-Galvis <galviscamilojuan@gmail.com>
2025-06-05 01:15:12 -0500

    making some sockets

--- src/game/__init__.py
+++ src/game/__init__.py
@@ -1,4 +1,5 @@
 from .gameService import GameEventsService
+from .gameObject import Game, GameState
 from fastapi import APIRouter, WebSocket, WebSocketException
 
 route = APIRouter(
@@ -12,14 +13,14 @@ gameManeger = GameEventsService()
 def createGame():
     return gameManeger.createGame()
 
-@route.websocket("/ws")
+@route.websocket("/addPlayer")
 async def addPlayer(websocket: WebSocket):
     try:
         await websocket.accept()
         print("conectado")
         data = await websocket.receive_json(mode="text")
-        print(data)
         await websocket.send_text('{"code": 1, "pawn": 15, "box": 101}')
         await websocket.close()
+
     except WebSocketException:
         print("se desconecto")
